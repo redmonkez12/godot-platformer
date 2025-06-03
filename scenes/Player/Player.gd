a6d718eb4be4b31b303038cfc1e30316643b2e5d
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2025-06-03 21:44:30 +0200

    Update animation

--- scenes/Player/Player.gd
+++ scenes/Player/Player.gd
@@ -2,11 +2,15 @@ extends CharacterBody2D
 
 class_name Player
 
+@export var fell_off_y: float = 800.0
+
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var debug_label: Label = $DebugLabel
 
 const GRAVITY: float = 690.0
 const JUMP_SPEED: float = -270.0
 const RUN_SPEED: float = 120.0
+const MAX_FALL: float = 350.0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -25,4 +29,20 @@ func _physics_process(delta: float) -> void:
 	if is_equal_approx(velocity.x, 0.0) == false:
 		sprite_2d.flip_h = velocity.x < 0
 	
+	velocity.y = clampf(velocity.y, JUMP_SPEED, MAX_FALL)
+	
 	move_and_slide()
+		
+	update_debug_label()
+	falling_off()
+
+func update_debug_label() -> void:
+	var ds: String = ""
+	ds += "Floor:%s\n" % [is_on_floor()]
+	ds += "V:%.1f,%.1f\n" % [velocity.x, velocity.y]
+	ds += "P:%.1f,%.1f" % [global_position.x, global_position.y]
+	debug_label.text = ds
+	
+func falling_off() -> void:
+	if global_position.y > fell_off_y:
+		queue_free()
